f156223935685569d08a46b97409e22e3babab01
Vahan <vahan99mk@gmail.com>
2020-03-17 23:35:23 +0400

    chart

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('superadmin.index');
+        $companies = Company::orderBy('created_at')->get()->groupBy(function ($group) {
+            return \Carbon\Carbon::parse($group->created_at)->format('Y-m-d');
+        })->toArray();
+        return view('superadmin.index', compact('companies'));
     }
 }
