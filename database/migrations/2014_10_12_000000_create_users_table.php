57272b37d5962de866f106854f47ad2a4e2333c9
Vahan <vahan99mk@gmail.com>
2020-04-06 14:37:08 +0400

    changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone')->unique();
+            $table->string('email');
+            $table->string('phone');
             $table->longText('address');
             $table->bigInteger('admin_id')->nullable();
             $table->boolean('status')->default(0);
